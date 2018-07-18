29580d82c772e96bd5b1347b0c96ef5d66ca45bf
JAKES <istakezx@gmail.com>
2018-07-17 22:02:21 -0400

    Paddle movement added
    Basic paddle movement complete

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoseCollider : MonoBehaviour {
+    [SerializeField] string gameOverScene;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+private void OnTriggerEnter2D(Collider2D collision)
+    {  
+         SceneManager.LoadScene(gameOverScene);
+    }
 }
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidth = 16f;
+    
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        float mouseX = Input.mousePosition.x / Screen.width * screenWidth;
+
+        Debug.Log(mouseX);   // return a % instead of a pixel value. 1.0 to the far right, 0.0 to the left then * 16 to get screen units
+        Vector2 paddlePos = new Vector2(mouseX, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
